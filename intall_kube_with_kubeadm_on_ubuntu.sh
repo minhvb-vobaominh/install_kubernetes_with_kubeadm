979f7a76515a66387f501563bdc9fa362bf8d460
minhvb-vobaominh <vobaominh.vbm@gmail.com>
2024-01-01 14:27:38 +0700

    done

--- intall_kube_with_kubeadm_on_ubuntu.sh
+++ intall_kube_with_kubeadm_on_ubuntu.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-echo "Install"
 sudo apt update
 sudo apt-get install -y software-properties-common curl
 
+# add the k8s repository
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list
+
+# add the cri-o repository
+curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/ /" | tee /etc/apt/sources.list.d/cri-o.list
+
+# install the pkgs
+apt-get update
+apt-get install -y cri-o kubelet kubeadm kubectl
